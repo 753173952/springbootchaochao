2209746502421639e01aa6ea919847d4fe3167fa
wcc <753173952@qq.com>
2018-12-01 20:59:59 +0800

    更新

--- src/main/java/com/appication/springtest/UserServiceTest.java
+++ src/main/java/com/appication/springtest/UserServiceTest.java
@@ -15,6 +15,6 @@ public class UserServiceTest {
 
     @Test
     public void testService(){
-        System.out.println(service+"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++====================");
+        System.out.println();
     }
 }
